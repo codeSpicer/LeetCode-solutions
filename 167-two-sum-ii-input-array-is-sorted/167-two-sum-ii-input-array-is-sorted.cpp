a3d05381e7449fde1e0c242ed035dbaef09dd8f9
Akshat kumar <77925154+codeSpicer@users.noreply.github.com>
2022-05-25 20:58:11 +0530

    Time: 7 ms (98.97%), Space: 15.6 MB (44.43%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.cpp
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.cpp
@@ -1,21 +1,35 @@
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& numbers, int target) {
-        vector<int> ans ;
+    vector<int> twoSum(vector<int>& num, int target) {
+        
+        // O(N logN ) approach using binary search
+            //      as the array is already sorted we can iterate the array 
+            //      and find target-current_value in the rest of array using binary search
+            //      it will be called n times in worst case and every call would be of log n time so nlogn
+
+        // linear approach using hash-map-uh        // 2 pass solution
+            //      we can add all the elements in the unordered_map
+            //      then taking the advantage of constant lookup times
+            //      we can easily iterate the array and find of complements with add up to the target number
+        
+        // the ultimate god of memory saving and one pass -> Two pointer approach
+            //     two pointers one at end one at start
+            //     if sum of them greater than target then end pointer -- else start pointer ++
+            //     EZZ
+        
         
         int i = 0;
-        int j = numbers.size()-1;
-        int sum = 0;
+        int j = num.size()-1;
         
-        while( i != j){
-            sum = numbers[i] + numbers[j];
+        vector<int> ans;
+        
+        while( i != j ){
             
-            if( sum == target){
+            if( num[i] + num[j] == target){
                 ans.push_back(i+1);
                 ans.push_back(j+1);
-            }
-            
-            if( sum > target){
+                break;
+            }else if( num[i] + num[j] > target){
                 j--;
             }else{
                 i++;
