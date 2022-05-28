2bb4adb1a128c03bc8f9a64792c4794e12b192a7
Akshat kumar <77925154+codeSpicer@users.noreply.github.com>
2022-05-28 15:10:47 +0530

    Time: 61 ms (34.61%), Space: 10.8 MB (16.02%) - LeetHub

---
+++ 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        
+        // general idea add everyting in a onordered map 
+        // then loop the array to find first element with freq 1
+        // return it
+        // time complexity O(n) space complexi o(n)
+        
+        unordered_map<int,int> mp ;
+        
+        for( int i = 0 ; i < s.length() ; i++){
+            mp[s[i]]++;
+        }
+        
+        int index = -1;
+        
+        for( int i = 0 ;i < s.length() ; i++){
+            if(mp[s[i]]==1){
+                return i;
+            }
+        }
+        
+        return index;
+        
+    }
+};
