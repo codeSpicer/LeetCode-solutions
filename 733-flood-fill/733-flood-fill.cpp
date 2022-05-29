fa1fcbf81510e9ad261d9f05f7e1ecaa9ab21699
Akshat kumar <77925154+codeSpicer@users.noreply.github.com>
2022-05-29 13:48:42 +0530

    Update 733-flood-fill.cpp

--- 733-flood-fill/733-flood-fill.cpp
+++ 733-flood-fill/733-flood-fill.cpp
@@ -2,30 +2,32 @@ class Solution {
 public:
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor) {
         
-        int oldColor  = image[sr][sc];
+        if( image[sr][sc] == newColor){ return image; }   // if pixel is already newcolor
         
-        dfs( image , sr , sc ,newColor , oldColor);        
+        fill( image , sr , sc , newColor , image[sr][sc]);  // depth first search
         
         return image;
+        
     }
     
-    void dfs( vector<vector<int>>& image, int sr, int sc, int newColor , int oldColor){
+    
+    void fill(vector<vector<int>>& image, int sr, int sc, int newColor, int initialColor){
         
-        if( sr <0 || sc <0  || sr >= image.size() || sc >= image[0].size() || image[sr][sc] != oldColor ||image[sr][sc] == newColor)           {
-            return;
+        // first check for out of bounds -> then check for initial colour check
+        
+        if( sr < 0 || sc < 0 || sr >= image.size() || sc >= image[0].size() || image[sr][sc] != initialColor ){
+            return;     // if anything is true then we return
         }
-            image[sr][sc] = newColor;
+        
+        image[sr][sc] = newColor;       // else we color the pixel to the new color
             
-            dfs( image , sr-1 , sc , newColor , oldColor);
-            dfs( image , sr+1 , sc , newColor , oldColor);
-            dfs( image , sr , sc-1 , newColor , oldColor);
-            dfs( image , sr , sc+1 , newColor , oldColor);
+        // and call this fucntion recursively and making it a depth first search
+        
+        fill( image , sr-1 , sc , newColor , initialColor);
+        fill( image , sr+1 , sc , newColor , initialColor);
+        fill( image , sr , sc-1 , newColor , initialColor);
+        fill( image , sr , sc+1 , newColor , initialColor);
             
-        
-        
-        
     }
-    
-    
     
 };
