c0739385c91b33c76631cb7bd57a649fa95394fc
Akshat kumar <77925154+codeSpicer@users.noreply.github.com>
2022-06-05 11:02:10 +0530

    Time: 74 ms (23.42%), Space: 36.9 MB (45.34%) - LeetHub

--- 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.cpp
+++ 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.cpp
@@ -19,25 +19,44 @@ public:
         // other approach is to make a sorted array from the bst
         // and then use 2 pointer approach to add up to target
         
-        unordered_set<int> set;
-        return dfs(root , set , k );
+        // unordered_set<int> set;
+        // return dfs(root , set , k );
+        
+        vector<int> arr;
+        inorder( root , arr);
+        
+        int i = 0; int j = arr.size()-1;
+        
+        while( i < j){
+            if( arr[i]+arr[j] > k){
+                j--;
+            }else if(arr[i]+arr[j] < k){
+                i++;
+            }else if(arr[i]+arr[j] == k){
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    void inorder( TreeNode* root , vector<int> &arr){
+        if( root == NULL){
+            return ;
+        }
+        inorder( root->left , arr);
+        arr.push_back(root->val);
+        inorder(root->right , arr);
     }
     
     bool dfs( TreeNode* root , unordered_set<int> &set , int k){         // set pass karte hue & laga dena :)
         
-        if( root == NULL){
-            return false;
-        }
+        if( root == NULL){return false;}
         
-        if( set.count(k - root->val)){
-            return true;
-        }
+        if( set.count(k - root->val)){return true;}
+        
         set.insert(root->val);
         
-        return dfs(root->left , set, k) || dfs( root->right , set , k);
-        
-        
-        
+        return dfs(root->left , set, k) || dfs( root->right , set , k);       
     }
     
 };
