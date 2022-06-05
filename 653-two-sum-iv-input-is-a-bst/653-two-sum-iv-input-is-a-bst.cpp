01b47a6f3ce3d5a02b8d583d460741ae0f4ae076
Akshat kumar <77925154+codeSpicer@users.noreply.github.com>
2022-06-05 10:53:55 +0530

    Time: 93 ms (9.67%), Space: 39 MB (25.51%) - LeetHub

---
+++ 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.cpp
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool findTarget(TreeNode* root, int k) {
+        
+        // one approach that works for non bst also is to use a hashset to store the nodes values 
+        // then search for the complement of that node in set
+        
+        // other approach is to make a sorted array from the bst
+        // and then use 2 pointer approach to add up to target
+        
+        unordered_set<int> set;
+        return dfs(root , set , k );
+    }
+    
+    bool dfs( TreeNode* root , unordered_set<int> &set , int k){         // set pass karte hue & laga dena :)
+        
+        if( root == NULL){
+            return false;
+        }
+        
+        if( set.count(k - root->val)){
+            return true;
+        }
+        set.insert(root->val);
+        
+        return dfs(root->left , set, k) || dfs( root->right , set , k);
+        
+        
+        
+    }
+    
+};
