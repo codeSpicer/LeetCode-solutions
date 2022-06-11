c36ad791d61ca82bb8e2cc3c1b00170e282bf7e2
Akshat kumar <77925154+codeSpicer@users.noreply.github.com>
2022-06-11 15:54:05 +0530

    Time: 129 ms (72.35%), Space: 33.8 MB (54.68%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
@@ -9,30 +9,44 @@ public:
         
         // time comp -> O(2N)
         // space cmp -> O(N);
+        int n = nums.size();
+		if(n == 0) return 0;
+		sort(nums.begin(), nums.end());
+		int cnt = 1, mx = 1;
+		for(int j = 1; j < n; j++) {
+			if(nums[j] == nums[j-1])
+				continue;
+			if(nums[j] == nums[j-1] + 1)
+				cnt++;
+			else 
+				cnt = 1;
+			mx = max(mx, cnt);
+		}
+		return mx;
         
-        unordered_set<int> set;
-        int Max=0;
-        int temp_max=0;
+//         unordered_set<int> set;
+//         int Max=0;
+//         int temp_max=0;
         
-        for( auto &n : nums ){
-            set.insert(n);
-        }
+//         for( auto &n : nums ){
+//             set.insert(n);
+//         }
         
-        for( auto &n : nums){
+//         for( auto &n : nums){
             
-            if( !set.count(n-1)){
-                // element is first in this sequence
-                int num = n;
-                temp_max++;
-                while( set.count(++num)){   // while next element in sequence is present
-                    temp_max++;
-                }
-                Max = max( temp_max , Max);
-                temp_max=0;
+//             if( !set.count(n-1)){
+//                 // element is first in this sequence
+//                 int num = n;
+//                 temp_max++;
+//                 while( set.count(++num)){   // while next element in sequence is present
+//                     temp_max++;
+//                 }
+//                 Max = max( temp_max , Max);
+//                 temp_max=0;
                 
-            }
+//             }
             
-        }
-        return Max;
+//         }
+//         return Max;
     }
 };
