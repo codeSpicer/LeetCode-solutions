5d2999c3c3453f80954c66e05c69638b463b481c
Akshat kumar <77925154+codeSpicer@users.noreply.github.com>
2022-06-17 14:17:02 +0530

    Time: 4 ms (21.86%), Space: 5.9 MB (22.62%) - LeetHub

--- 374-guess-number-higher-or-lower/374-guess-number-higher-or-lower.cpp
+++ 374-guess-number-higher-or-lower/374-guess-number-higher-or-lower.cpp
@@ -14,21 +14,21 @@ public:
         int low = 0;
         int high = n;
         
-        while( low < high){
+        while( low <= high){
             
             int mid = low + (high-low)/2;
             
-            if( guess(mid) == -1){
-                high= mid-1;
-            }else if( guess(mid) == 1){
-                low = mid+1;
-            }else{
+            int res = guess(mid);
+            if (res == 0)
                 return mid;
-            }
+            else if (res < 0)
+                high = mid - 1;
+            else
+                low = mid + 1;
             
         }
         
-        return low;
+        return -1;
         
     }
 };
