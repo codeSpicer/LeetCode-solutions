0956045cee4803b5378bea7074f995390b12d7f8
Akshat kumar <77925154+codeSpicer@users.noreply.github.com>
2022-06-18 15:29:14 +0530

    Time: 100 ms (49.23%), Space: 48.4 MB (77.63%) - LeetHub

--- 55-jump-game/55-jump-game.cpp
+++ 55-jump-game/55-jump-game.cpp
@@ -2,6 +2,12 @@ class Solution {
 public:
     bool canJump(vector<int>& nums) {
         
+        // greedy approach
+        // we iterate from the end of the array
+        // taking the last element as target
+        // if the element before it can reach the target element then we make that previous element the target
+        // if the target is accessible from first index then first index will become target
+        
         int n = nums.size()-1;
         int target = n;
         
