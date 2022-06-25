052f82f7901960792038eb9616057fbfb0e97cc0
Akshat kumar <77925154+codeSpicer@users.noreply.github.com>
2022-06-25 13:12:11 +0530

    Time: 4 ms (23.89%), Space: 5.8 MB (89.10%) - LeetHub

--- 1108-defanging-an-ip-address/1108-defanging-an-ip-address.cpp
+++ 1108-defanging-an-ip-address/1108-defanging-an-ip-address.cpp
@@ -3,17 +3,10 @@ public:
     string defangIPaddr(string address) {
         
         string res;
-        
-        for( int i = 0 ;i < address.length()  ; i++){
-            
-            if( address[i] == '.'){
-                res.push_back('[');
-                res.push_back('.');
-                res.push_back(']');
-            }else{
-                res.push_back(address[i]);
-            }
-            
+        for(auto c: address) {
+            if(c=='.') {
+                res+="[.]";
+            } else res+=c;
         }
         return res;
     }
