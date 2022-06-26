4d8b4c31591a2630b4e4b56422c936e8e0984127
Akshat kumar <77925154+codeSpicer@users.noreply.github.com>
2022-06-26 23:21:12 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (72.37%) - LeetHub

--- 441-arranging-coins/441-arranging-coins.cpp
+++ 441-arranging-coins/441-arranging-coins.cpp
@@ -22,7 +22,7 @@ public:
             
             if( coins > n){
                 high = mid-1;
-            }else{
+            }else if( coins < n ){
                 low = mid+1;
             }
             
