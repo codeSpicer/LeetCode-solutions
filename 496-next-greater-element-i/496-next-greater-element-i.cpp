80cc3bfdb98e593a015806ff3bf8659d0712e153
Akshat kumar <77925154+codeSpicer@users.noreply.github.com>
2022-07-07 11:28:47 +0530

    Time: 3 ms (97.13%), Space: 8.7 MB (76.14%) - LeetHub

--- 496-next-greater-element-i/496-next-greater-element-i.cpp
+++ 496-next-greater-element-i/496-next-greater-element-i.cpp
@@ -2,31 +2,32 @@ class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
         
-        vector<int> result;
+        vector<int> result(nums1.size() , -1);
         
+        // idea is to use a monotonic stack approach 
+        // and help of hashmap to store the indexes of original array element 
+        
+        unordered_map<int,int> mp;
         for( int i = 0 ; i < nums1.size() ; i++){
+            mp[nums1[i]] = i;                       // key => element, value => index of element 
+        }
+        
+        stack<int> st;
+        
+        for( int i = 0 ;i < nums2.size() ; i++){
             
-            int greater = -1;
-            int index = 0;
-            
-            while( nums2[index] != nums1[i] ){
-                index++;
+            while( st.size() && nums2[i] > st.top()){     // when a greater element comes we pop all top elements smaller than current element and assign value in result array 
+                result[mp[st.top()]] = nums2[i];
+                st.pop();
             }
-            
-            for( int j = index ; j < nums2.size() ; j++){
-                
-                if( nums2[j] > nums1[i]){
-                    greater = nums2[j];
-                    break;
-                }
-                
+            if( mp.find(nums2[i]) != mp.end() ){
+                st.push(nums2[i]);                  // but if the current element is not in array 1 then we dont push it in stack
             }
-            
-            result.push_back( greater);
             
         }
         
-        return result;
         
+            
+        return result;
     }
 };
