5a61daa428d846b6cd30002ecc33bf3a302bdfd8
Akshat kumar <77925154+codeSpicer@users.noreply.github.com>
2023-02-21 00:35:04 +0530

    Time: 8 ms (60.19%), Space: 10.9 MB (60.39%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,52 +1,25 @@
 class Solution {
 public:
-    // for the sake of convience, not to include again and again in my function call, I declare target here
-    int target;
-    
-    vector<vector<int>> ans; // 2-D vector to store our answer
-    
-    void solve(vector<int>& arr, int i, int sum, vector<int> op)
-    {
-        // if i crosses the array size, we will return saying that no more possibilty is left to choose
-        
-        if(i >= arr.size()) 
-        {
-            return;
-        }
-        
-        // if value at ith index + sum becomes equal to target, then we will store it in our answer array, saying that yes it is a possible combination
-        if(arr[i] + sum == target)
-        {
-            op.push_back(arr[i]);
-            ans.push_back(op);
-            return;
-        }
-        
-        // if value at ith index + sum is less than target, then we have two choices i.e whether to include this value in our possible combiation array or not include that,
-        if(arr[i] + sum < target)
-        {
-            // we make two output vector, one for calling function at same index and anthor for calling function frm next index. Because for every element we have unlimited choices, that it will contribute in making our sum any number of times.
-           
-            
-            op.push_back(arr[i]);
-            solve(arr, i, sum + arr[i], op);
-            op.pop_back();
-            solve(arr, i + 1, sum, op);
-        }
-        else
-        {
-            solve(arr, i + 1, sum, op); // call for the next index
-        }
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>> combinations;
+        vector<int> combination;
+        sort(candidates.begin(), candidates.end());
+        dfs(combinations, combination, candidates, 0, target);
+        return combinations;
     }
-    
-    vector<vector<int>> combinationSum(vector<int>& arr, int required_target) {
-        ans.clear(); //clear global array, make to sure that no garbage value is present in it
+
+    void dfs(vector<vector<int>>& combinations, vector<int>& combination, vector<int>& candidates, int index, int target) {
+        if(target < 0 ) return;
+
+        if(target == 0) {
+            combinations.push_back(combination);
+            return;
+        }
         
-        target = required_target; // give target what he wants 
-        vector<int> op; // op array to try all possible combination
-        sort(arr.begin(),arr.end()); // sort the array in ascending order
-        solve(arr, 0, 0, op); // call function
-        
-        return ans; // return the final answer array
+        for(int i = index; i < candidates.size(); ++i) {
+            combination.push_back(candidates[i]);
+            dfs(combinations, combination, candidates, i, target - candidates[i]);
+            combination.pop_back();
+        }
     }
 };
