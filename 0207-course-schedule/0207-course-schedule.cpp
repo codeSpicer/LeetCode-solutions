12b7602cdf4b7f9f36ef8404d0bcc74c960d1f28
Akshat kumar <77925154+codeSpicer@users.noreply.github.com>
2023-04-01 16:02:52 +0530

    Time: 28 ms (34.53%), Space: 13.1 MB (95.42%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,65 +1,36 @@
 class Solution {
-private:    
-    
-//     bool detectCycle( int node , int parent , vector<int> adj[] , vector<bool> &visited ){
-        
-//         visited[node] = true;
-        
-//         for( const auto& neighbour : adj[node]){
-            
-//             if( !visited[neighbour] ){
-//                 if( detectCycle(neighbour , node , adj , visited) == true ){
-//                     return true;
-//                 }
-//             }else if( neighbour != parent ){
-//                 return true;
-//             }
-//         }
-//         return false;
-//     }
-    
 public:
-//     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        queue<int> q;
+        int count = 0;
+        vector<int> indegree(numCourses, 0);
         
-//         vector<int> adj[numCourses];    // adj list of course
-        
-//         for( const auto& edge : prerequisites){
-//             adj[edge[1]].push_back(edge[0]);
-//         }
-        
-//         vector<bool> visited(numCourses , false);
-        
-//         for( int i = 0 ; i < numCourses ; i++){
-//             if( detectCycle( ))
-//         }
-        
-//     }
-    
-    
-    bool canFinish(int n, vector<vector<int>>& pre) {
-        vector<int> adj[n];
-        for(auto edge : pre)
-            adj[edge[1]].push_back(edge[0]);
-        vector<int> vis(n,0);
-        
-        for(int i=0;i<n;i++){
-            if(iscycle(adj,vis,i))
-                return false;
+        vector<int> adj[numCourses];         // adj list        
+        int edges = prerequisites.size();
+        for(int i = 0;i<edges;i++){
+            int u = prerequisites[i][0];
+            int v = prerequisites[i][1];
+            adj[v].push_back(u);
         }
-        return true;
-    }
-    
-    bool iscycle(vector<int> adj[],vector<int> &vis,int id){
-        if(vis[id]==1)
-            return true;
-        if(vis[id]==0){
-            vis[id]=1;
-            for(auto edge : adj[id]){
-                if(iscycle(adj,vis,edge))
-                    return true;
+        
+        for(int i=0; i<numCourses; i++){
+            for(auto it : adj[i])
+                indegree[it]++;
+        }
+        for(int i=0; i<numCourses; i++){
+            if(indegree[i] == 0)
+                q.push(i);
+        }
+        while(!q.empty()){
+            count++;
+            int node = q.front();
+            q.pop();
+            for(int i: adj[node]){
+                indegree[i]--;
+                if(indegree[i] == 0)
+                    q.push(i);
             }
         }
-        vis[id] = 2;
-        return false;
-    }
+        return count == numCourses;
+  }
 };
