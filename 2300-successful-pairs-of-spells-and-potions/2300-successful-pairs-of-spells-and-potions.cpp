11ce4dbf561ab67a76ea93c08097f9b80ead11a1
Akshat kumar <77925154+codeSpicer@users.noreply.github.com>
2023-04-02 20:49:38 +0530

    Time: 228 ms (91.83%), Space: 97.1 MB (86.98%) - LeetHub

--- 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.cpp
+++ 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.cpp
@@ -1,26 +1,24 @@
 class Solution {
 public:
     vector<int> successfulPairs(vector<int>& spells, vector<int>& potions, long long success) {
-        //  for every spell we will try to find the index at which , its product with potions is larger than success 
-        // then for all the indexes in the sorted array , next elements product will be larger too
-        // so potions.size() - index will be stored in ans vector
         
-        sort(potions.begin(), potions.end());
-        vector<int> answer;
+        sort( potions.begin() , potions.end());
+        vector<int> ans(spells.size()); 
         
-        int m = potions.size();
-        int maxPotion = potions[m - 1];
-        
-        for (auto& spell : spells) {
-            // Minimum value of potion whose product with current spell  
-            // will be at least success or more.
+        for( int i = 0 ; i < spells.size() ; i++){
             
-            long long minPotion = ceil((1.0 * success) / spell);
+            //  for every spell we will try to find the index at which , its product with potions is larger than success 
+            // then for all the indexes in the sorted array , next elements product will be larger too
+            // so potions.size() - index will be stored in ans vector
             
-            auto index = lower_bound(potions.begin(), potions.end(), minPotion) - potions.begin();
-            answer.push_back(m - index);
+            long long target = ceil( (success*1.0) / spells[i] );
+            
+            
+            ans[i] = potions.size() - (lower_bound( potions.begin() , potions.end() , target) - potions.begin());
+            
         }
         
-        return answer;
+        return ans;
+        
     }
 };
