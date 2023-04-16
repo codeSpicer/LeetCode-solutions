3c6bc041f37d867e7a02c47f86067b7801f9d6a6
Akshat kumar <77925154+codeSpicer@users.noreply.github.com>
2023-04-16 23:16:15 +0530

    Time: 12 ms (23.23%), Space: 13.7 MB (16.76%) - LeetHub

---
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:    
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        
+        set<vector<int>> ans;       // stores non duplicate combos
+        vector<int> temp ;          // temp array
+        sort( candidates.begin() , candidates.end());   // sorting the original array so we can skip the same values
+        
+        backtrack( candidates , target , 0 , temp , ans);
+        
+        vector<vector<int>> result( ans.begin() , ans.end() ) ;     // transfering all the elements from set to resultant vector
+        return result;        
+        
+    }
+    
+    void backtrack( vector<int> candidates , int target , int index , vector<int> &temp , set<vector<int>>& ans){
+        
+        if( target == 0){
+            ans.insert( temp);  // add to answer and return
+            return;
+        }
+        
+        for( int i = index ; i < candidates.size() ; i++){
+            
+            if( i > index && candidates[i] == candidates[i-1]) continue;        // TO AVOID TAKING SAME ELEMENTS IN THE ANS AGAIN  
+            if( candidates[i] > target) break;          // OPTIMIZATION // IF ELEMENT IS GREATER THAN TARGET THEN ALL UPCOMING ELEMENTS ARE INVALID
+            
+            temp.push_back( candidates[i]);
+            backtrack( candidates , target-candidates[i] , i+1 , temp , ans);   // to pick or not to pick
+            temp.pop_back();
+        }        
+    }
+    
+};
