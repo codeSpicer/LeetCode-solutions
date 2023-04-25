0b36ff1898d1d431b45d780e3922813529bb18e0
Akshat kumar <77925154+codeSpicer@users.noreply.github.com>
2023-04-25 15:59:10 +0530

    Time: 3 ms (94.08%), Space: 13.7 MB (83.63%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    
+    /*
     int recurse( vector<int> &dp, vector<int> cost , int indx ){
         
         if( indx == 1 || indx == 0){
@@ -15,11 +15,22 @@ public:
         return dp[indx];
         
     }
+    */
     
     int minCostClimbingStairs(vector<int>& cost) {
         int n = cost.size();
         vector<int> dp( cost.size() , -1);
-        return min( recurse( dp,cost , n-1 ) , recurse(dp, cost , n-2));
+        
+        //return min( recurse( dp,cost , n-1 ) , recurse(dp, cost , n-2));
+        
+        for( int i = 0 ; i < n ; i++){
+            if( i < 2 ) dp[i] = cost[i];
+            else{
+                dp[i] = min( dp[i-1] , dp[i-2])+ cost[i];
+            }
+        }
+        
+        return min(dp[n-1],dp[n-2]);
         
     }
 };
